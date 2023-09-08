0b9c23a82b0efcbedefadc868a173cd5f9f067b5
mumo-esther <mumoesther2021@gmail.com>
2023-09-08 12:07:08 +0300

    Move counterslice to features

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -7,7 +7,7 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
+} from '../../features/counterSlice';
 import styles from './Counter.module.css';
 
 export function Counter() {
