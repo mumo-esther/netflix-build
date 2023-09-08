ba8b9e082594e0dcbf73d5504d3bc3f24d356c05
mumo-esther <mumoesther2021@gmail.com>
2023-09-08 15:13:39 +0300

    Fetch banner component content

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import './Banner.css'
+import axios from './axios';
+import requests from './Requests'
 
 function Banner() {
+
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZWZjZmYyOGY1ZjRiYWNkOTJlODRhMDFiODRkOGQzMiIsInN1YiI6IjY0ZmFlNWZmZTBjYTdmMDE0ZjZlMzYxZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mGT9BUxlQAJvz3r_LKKw7DEWKiFracxKj8vp_mYxsG0'; // Replace 'YOUR_ACCESS_TOKEN' with your actual access token
+
+      const request = await axios.get(requests.fetchNetflixOriginals, {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`, // Include the access token in the Authorization header
+        },
+      });
+
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
 
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n + 1) + '...' : string
@@ -9,25 +33,16 @@ function Banner() {
   return (
     <header className='banner' style={{
       backgroundSize: 'cover',
-      backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+      backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
       backgroundPosition: 'center'
     }}>
       <div className='banner_content'>
-        <h1 className='banner_title'>Movie Name</h1>
+        <h1 className='banner_title'>{movie?.name || movie?.title || movie?.original_name} </h1>
         <div className='banner_buttons'>
           <button className='banner_button'>Play</button>
           <button className='banner_button'>My List</button>
         </div>
-        <h1 className='banner_description'>{truncate(`In the tranquil heart of a bustling city, 
-        where the relentless rhythm of life never seems to cease, there exists a hidden oasis of serenity. 
-        A small park, adorned with vibrant blossoms and ancient trees stands as a refuge from the chaos of urban existence.
-         The gentle murmur of a crystal-clear stream meanders through the park, its waters reflecting the dappling sunlight like liquid diamonds.
-          Aromatic scents from the surrounding flowers dance on the breeze, captivating the senses and momentarily transporting visitors to a world untouched by the demands of time.
-          As the day unfolds people from all walks of life gather here seeking respite from their daily struggles.Children chase colorful butterflies, 
-          their laughter echoing through the air like the sweetest melody.Elderly couples find solace on weathered benches reminiscing about a lifetime of shared adventures.
-          Young lovers steal moments of intimacy beneath the shade of ancient oak trees, their whispered promises carrying the weight of eternity.
-          This park, a testament to the enduring beauty of nature amidst the modern chaos, serves as a reminder that even in the most hectic of lives, 
-          moments of tranquility and connection with the world around us are waiting to be discovered.`, 150)}</h1>
+        <h1 className='banner_description'>{truncate(movie?.overview, 150)}</h1>
       </div>
       <div className='banner--fadeBottom'></div>
     </header >
