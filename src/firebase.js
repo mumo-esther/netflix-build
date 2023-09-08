585f459c2a21ba1cb8c4f294f603b92dd181b6a6
mumo-esther <mumoesther2021@gmail.com>
2023-09-08 12:06:32 +0300

    Add firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCvEn-qM5Cn1uP-sJXglqDdOs12CTTvDEM",
+    authDomain: "netflix-build-14c63.firebaseapp.com",
+    projectId: "netflix-build-14c63",
+    storageBucket: "netflix-build-14c63.appspot.com",
+    messagingSenderId: "217670495295",
+    appId: "1:217670495295:web:8724d1a3885f8bffcd1c8a",
+    measurementId: "G-5K2S183CFN"
+  };
