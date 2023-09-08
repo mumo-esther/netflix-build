a884838d9aba16c6e6d171fc1f5112b379bf86ee
mumo-esther <mumoesther2021@gmail.com>
2023-09-08 15:14:33 +0300

    Update Homescreen

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './HomeScreen.css'
 import Nav from './Nav'
 import Banner from './Banner'
+import Row from './Row'
+import requests from './Requests'
 
 function HomeScreen() {
   return (
@@ -9,8 +11,16 @@ function HomeScreen() {
       <Nav />
 
       <Banner />
-
-        {/*Row */}
+      <Row title='NETFLIX ORIGINALS'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow />
+      <Row title='Trending Now' fetchUrl={requests.fechTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
