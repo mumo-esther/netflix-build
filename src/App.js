0ce941a3bffe5a846808287262894b30d6e1b0eb
mumo-esther <mumoesther2021@gmail.com>
2023-09-08 20:21:56 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import {Routes, Route} from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Routes>
+        {!user ? (
+          <Route path="/" element={<LoginScreen />} />
+        ): <Route path="/" element={<HomeScreen />} /> }
+        
+      </Routes>
     </div>
   );
 }
