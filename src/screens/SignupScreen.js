e0b97c32fe400e2958504e39bfa7a4c518bb6f8a
mumo-esther <mumoesther2021@gmail.com>
2023-09-09 17:22:20 +0300

    Created and authorized signin using firebase

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,54 @@
+import React, { useRef } from 'react'
+import './SignupScreen.css'
+import { auth } from '../firebase';
+
+function SignupScreen() {
+const emailRef =useRef(null);
+const passwordRef =useRef(null);
+
+const register = (e) => {
+    e.preventDefault();
+    auth.createUserWithEmailAndPassword(
+    emailRef.current.value,
+    passwordRef.current.value
+    ).then((authUser) => {
+        console.log(authUser)
+    })
+    .catch((error) => {
+        alert(error.message)
+    })
+}
+
+const signIn = (e) => {
+    e.preventDefault();
+
+    auth.signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+    ).then((authUser) => {
+        console.log(authUser)
+    }).catch((error) => {
+        alert(error.message)
+    })
+}
+
+    return (
+        <>
+            <div className="signupScreen">
+            <h1>Sign In</h1>
+                <form >
+               
+                    <input ref={emailRef} type="email" placeholder='Email' required />
+                    <input ref={passwordRef} type="password" placeholder='Password' required />
+                    <button onClick={signIn} type="submit">Sign In</button>
+                    <h4>
+                        <span className='signupScreen_gray'>New to Netflix?</span>
+                        <span onClick={register} className='signupScreen_link'>Sign up now.</span>
+                    </h4>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default SignupScreen
